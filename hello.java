b828cd59b22ebed9533466238083a9e494f5f159
AryaPeshkar2002 <100234122+AryaPeshkar2002@users.noreply.github.com>
2024-03-27 23:51:36 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello{
+  public static void main(String[] agrs)
+  {
+    System.out.printoutln("*************************");
+    System.out.println("hello from github");
+    System.out.println("*************************");
+  }
+}
+                          
+                          
+    
